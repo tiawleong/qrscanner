165f3bda14cc04f4d1f046ef924999a6e0932f13
Tiaw Leong <14835698+tiawleong@users.noreply.github.com>
2020-01-22 18:58:07 +0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -10,8 +10,8 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      alert(content: content);
       self.scans.unshift({ date: +(Date.now()), content: content });
+      alert(content);
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
