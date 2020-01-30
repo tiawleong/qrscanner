eccc6c5b3f09767948945b7fb37148c4fa9c2ccd
Tiaw Leong <14835698+tiawleong@users.noreply.github.com>
2020-01-30 13:06:20 +0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -8,7 +8,7 @@ var app = new Vue({
   },
   mounted: function () {
     var self = this;
-    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 4, mirror:false });
+    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 4, mirror:true });
     self.scanner.addListener('scan', function (content, image) {
       self.scans.unshift({ date: +(Date.now()), content: content });
       alert(content);
